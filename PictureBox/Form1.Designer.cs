8bb17a60105ebf3966b6256f16ca47fdc96fd22e
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-11-16 20:06:46 +0900

    PictureBox

---
+++ PictureBox/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿
+namespace PictureBox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureTest = new System.Windows.Forms.PictureBox();
+            this.buttonStart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTest)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureTest
+            // 
+            this.pictureTest.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureTest.Location = new System.Drawing.Point(0, 0);
+            this.pictureTest.Name = "pictureTest";
+            this.pictureTest.Size = new System.Drawing.Size(800, 450);
+            this.pictureTest.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureTest.TabIndex = 0;
+            this.pictureTest.TabStop = false;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(713, 415);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(75, 23);
+            this.buttonStart.TabIndex = 1;
+            this.buttonStart.Text = "button1";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonStart);
+            this.Controls.Add(this.pictureTest);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureTest)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureTest;
+        private System.Windows.Forms.Button buttonStart;
+    }
+}
+
---
+++ PictureBox/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            Graphics picGr;
+            Pen penTmp;
+
+            int i;
+            int r, g, b;
+
+            Random rnd = new Random();
+
+            r = rnd.Next(256);
+            g = rnd.Next(256);
+            b = rnd.Next(256);
+
+            penTmp = new Pen(Color.FromArgb(r, g, b), 1);
+
+            picGr = pictureTest.CreateGraphics();
+
+            for(i = 0; i < pictureTest.Width; i++)
+            {
+                picGr.DrawLine(penTmp, i, 0, i, pictureTest.Height);
+                System.Threading.Thread.Sleep(10);
+                Application.DoEvents();
+            }
+        }
+    }
+}
