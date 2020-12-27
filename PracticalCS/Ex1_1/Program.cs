927a3f02e7dc604eca8b055f553857afba75b726
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-12-27 21:10:57 +0900

    프랙티컬 C# 경로생성

---
+++ PracticalCS/Ex1_1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ex1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
