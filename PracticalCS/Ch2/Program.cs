8e2efb24244fbc562132fb5747c4f0f9ad6c6c6b
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-12-27 23:03:43 +0900

    2장 FeetToMeter 구현

---
+++ PracticalCS/Ch2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Ch2
+{
+    class Program
+    {
+        static double FeetToMeter(int feet)
+        {
+            return feet * 0.3048;
+        }
+        static void Main(string[] args)
+        {
+            for (int feet = 1; feet < 11; feet++)
+            {
+                Console.WriteLine("{0} ft = {1:0.0000} m", feet, FeetToMeter(feet));
+            }
+                
+        }
+    }
+}
