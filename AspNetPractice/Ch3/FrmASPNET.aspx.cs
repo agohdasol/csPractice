f3e4de1b20cf9de29e1334ba2119cc048de4e9f2
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-12-29 21:06:46 +0900

    ASP.NET 3장

---
+++ AspNetPractice/Ch3/FrmASPNET.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ch3
+{
+    public partial class FrmASPNET : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            for(int i=0;i<11;i++)
+                Response.Write($"새로운문구{i}<br/>");
+        }
+    }
+}
