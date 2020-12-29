8f4f7507979f23dfa178dcf96b503578aac02459
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2020-12-30 01:00:42 +0900

    ASP.NET ~69p

---
+++ AspNetPractice/Ch3/FrmResponseRedirect.aspx
@@ -0,0 +1,26 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="FrmResponseRedirect.aspx.cs" Inherits="Ch3.FrmResponseRedirect" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Response 개체</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="BtnDotNetKorea" runat="server"
+                Text="닷넷코리아" OnClick="BtnDotNetKorea_Click" />
+            <br />
+            <asp:LinkButton ID="BtnDevLec" runat="server"
+                OnClick="BtnDevLec_Click">데브렉</asp:LinkButton>
+            <br />
+            <asp:ImageButton ID="BtnVisualAcademy" runat="server"
+                AlternateText="비주얼아카데미" ToolTip="하와소"
+                OnClick="BtnVisualAcademy_Click" Style="height: 16px" />
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ AspNetPractice/Ch3/FrmResponseRedirect.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ch3
+{
+    public partial class FrmResponseRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void BtnDotNetKorea_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("http://www.dotnetkorea.com/");
+        }
+        protected void BtnDevLec_Click(object sender, EventArgs e)
+        {
+            string strURL = "http://www.devlec.com/";
+            Response.Redirect(strURL);
+        }
+        protected void BtnVisualAcademy_Click(object sender, ImageClickEventArgs e)
+        {
+            string strURL = String.Format(
+                "http://{0}/{1}?{2}={3}",
+                "www.visualacademy.com",
+                "default.htm",
+                "UserID", "RedPlus");
+            Response.Redirect(strURL);
+        }
+    }
+}
