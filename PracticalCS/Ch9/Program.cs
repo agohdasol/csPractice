a666b0323d7d100579b08168de7e39030cd152ac
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2021-01-04 21:53:45 +0900

    프랙티컬 C# ~9장

---
+++ PracticalCS/Ch9/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace Ch9
+{
+    class Program
+    {
+        public static void ChangeCurrentDirectory()
+        {
+            var workdir = Directory.GetCurrentDirectory();
+            Console.WriteLine(workdir);
+
+            Directory.SetCurrentDirectory(@"D:\csPractice\PracticalCS\Ch9\bin\Debug\netcoreapp3.12");
+            var newWorkdir = Directory.GetCurrentDirectory();
+            Console.WriteLine(newWorkdir);
+        }
+        public static void GetDirectoriesInServer()
+        {
+            //서버 디렉토리 구하기
+            var di = new DirectoryInfo(@"\\192.168.123.218\자료실\담당자\황다솔");
+            DirectoryInfo[] directories = di.GetDirectories("*", SearchOption.AllDirectories);
+            foreach (var dinfo in directories)
+            {
+                Console.WriteLine(dinfo.FullName);
+            }
+        }
+        static void Main()
+        {
+            GetDirectoriesInServer();
+        }
+    }
+}
---
+++ PracticalCS/Ex9/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Ex9
+{
+    class Program
+    {
+        //9.1 - 1
+        public static int CountClass(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                using (var reader = new StreamReader(filePath))
+                {
+                    int counter = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        if (line.Contains(" class "))
+                            counter++;
+                    }
+                    return counter;
+                }
+            }
+            return 0;
+        }
+        //9.1 - 2
+        public static int CountClassAllLines(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadAllLines(filePath);
+                int counter = 0;
+                foreach (var line in lines)
+                {
+                    if (line.Contains(" class "))
+                        counter++;
+                }
+                return counter;
+            }
+            return 0;
+        }
+        //9.1 - 3
+        public static int CountClassLines(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                var lines = File.ReadLines(filePath);
+                int counter = 0;
+                foreach (var line in lines)
+                {
+                    if (line.Contains(" class "))
+                        counter++;
+                }
+                return counter;
+            }
+            return 0;
+        }
+        //9.2
+        public static void AddIndexOfFile(string filePath)
+        {
+            var lines = File.ReadLines(filePath)
+                            .Select((s, n) => string.Format("{0} : {1}", n + 1, s));
+            var newFilePath = Path.ChangeExtension(filePath, ".txt");
+            File.WriteAllLines(newFilePath, lines);
+        }
+        public static void AddAfterEndOfLines(string toFilePath, string fromFilePath)
+        {
+            var lines = File.ReadAllLines(fromFilePath);
+            using(var writer=new StreamWriter(toFilePath, append: true))
+            {
+                foreach (var line in lines)
+                    writer.WriteLine(line);
+            }
+        }
+        static void Main()
+        {
+            //Console.WriteLine(CountClass(@"D:\csPractice\PracticalCS\Ex9\Program.cs"));
+            //Console.WriteLine(CountClassAllLines(@"D:\csPractice\PracticalCS\Ex9\Program.cs"));
+            //Console.WriteLine(CountClassLines(@"D:\csPractice\PracticalCS\Ex9\Program.cs"));
+            AddIndexOfFile(@"D:\csPractice\PracticalCS\Ex9\Program.cs");
+        }
+    }
+}
