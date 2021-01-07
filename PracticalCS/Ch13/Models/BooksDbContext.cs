a3acc83a06b0214b3b5093ebe6fea6dec204fa34
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2021-01-08 01:04:11 +0900

    프랙티컬 C# ~13장

--- PracticalCS/Ch13/Models/BooksDbContext.cs
+++ PracticalCS/Ch13/Models/BooksDbContext.cs
@@ -15,6 +15,7 @@ namespace Ch13.Models
         public BooksDbContext()
             : base("name=BooksDbContext")
         {
+            Database.SetInitializer(new MigrateDatabaseToLatestVersion<BooksDbContext, Configuration>());
         }
         public DbSet<Book> Books { get; set; }
         public DbSet<Author> Authors { get; set; }
--- PracticalCS/Ch13/Program.cs
+++ PracticalCS/Ch13/Program.cs
@@ -40,10 +40,114 @@ namespace Ch13
                 db.SaveChanges();
             }
         }
+        static IEnumerable<Book> GetBooks()
+        {
+            using(var db=new BooksDbContext())
+            {
+                return db.Books
+                    .Include(nameof(Author))
+                    .ToList();
+            }
+        }
+        static void DisplayAllBooks()
+        {
+            var books = GetBooks();
+            foreach (var book in books)
+                Console.WriteLine($"{book.Title} {book.PublishedYear} {book.Author.Name}");
+            Console.ReadLine();
+        }
+        static void AddAuthors()
+        {
+            using(var db=new BooksDbContext())
+            {
+                var author1 = new Author
+                {
+                    Birthday = new DateTime(1947, 09, 21),
+                    Gender = "M",
+                    Name = "스티븐 킹"
+                };
+                db.Authors.Add(author1);
+                var author2 = new Author
+                {
+                    Birthday = new DateTime(1911, 09, 19),
+                    Gender = "F",
+                    Name = "윌리엄 골딩"
+                };
+                db.Authors.Add(author2);
+                db.SaveChanges();
+            }
+        }
+        static void AddBooks()
+        {
+            using(var db=new BooksDbContext())
+            {
+                var author1 = db.Authors.Single(a => a.Name == "스티븐 킹");
+                var book1 = new Book
+                {
+                    Title = "샤이닝",
+                    PublishedYear = 1977,
+                    Author = author1
+                };
+                db.Books.Add(book1);
+                var author2 = db.Authors.Single(a => a.Name == "애거사 크리스티");
+                var book2 = new Book
+                {
+                    Title = "밀물을 타고",
+                    PublishedYear = 1843,
+                    Author = author2
+                };
+                db.Books.Add(book2);
+                var author3 = db.Authors.Single(a => a.Name == "찰스 디킨스");
+                var book3 = new Book
+                {
+                    Title = "크리스마스 캐럴",
+                    PublishedYear = 1843,
+                    Author = author3
+                };
+                db.Books.Add(book3);
+                var author4 = db.Authors.Single(a => a.Name == "윌리엄 골딩");
+                var book4 = new Book
+                {
+                    Title = "파리대왕",
+                    PublishedYear = 1954,
+                    Author = author4
+                };
+                db.Books.Add(book4);
+                db.SaveChanges();
+            }
+        }
+        //13.11
+        static void UpdateBook()
+        {
+            using(var db=new BooksDbContext())
+            {
+                var book = db.Books.Single(x => x.Title == "별의 계승자");
+                book.PublishedYear = 2016;
+                db.SaveChanges();
+            }
+        }
+        //13.12
+        static void DeleteBook()
+        {
+            using(var db=new BooksDbContext())
+            {
+                var book = db.Books.SingleOrDefault(x => x.Id == 10);
+                if (book != null)
+                {
+                    db.Books.Remove(book);
+                    db.SaveChanges();
+                }
+            }
+        }
         static void Main()
         {
-            InsertBooks();
-            Console.WriteLine("qwe");
+            var bookOfLongestName = GetBooks()
+                    .Where(book => book.Title.Length == GetBooks().Max(b => b.Title.Length));
+            foreach (var item in bookOfLongestName)
+            {
+                Console.WriteLine(item.Title);
+            }
+            
         }
     }
 }
