7cfac6cb25711b6803c7540b3e360faa8e64e037
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2021-01-27 19:53:59 +0900

    ASP.NET ~21장

---
+++ AspNetPractice/DotNetNote.Database/dbo/Tables/Maxims.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Maxims]
+(
+	[Id]						INT PRIMARY KEY	NOT NULL Identity(1,1),
+	[Name]					NVarChar(25)		Not Null,
+	[Content]				NVarChar(255)		Null,
+	[CreationDate]	DateTime	Default(GetDate())
+)
+Go
---
+++ DevWebAPI/Controllers/DefaultController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DevWebAPI.Controllers
+{
+  public class DefaultController : ApiController
+  {
+    // GET api/<controller>
+    public IEnumerable<string> Get()
+    {
+      return new string[] { "옴뇸뇸", "밍밍밍" };
+    }
+
+    // GET api/<controller>/5
+    public string Get(int id)
+    {
+      return "입력한 값" + id.ToString();
+    }
+
+    // POST api/<controller>
+    public void Post([FromBody] string value)
+    {
+    }
+
+    // PUT api/<controller>/5
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // DELETE api/<controller>/5
+    public void Delete(int id)
+    {
+    }
+  }
+}
