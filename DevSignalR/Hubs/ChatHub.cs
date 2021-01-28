293cee202ddd95194fe18ccb3cbc356294011d3c
DESKTOP-I49D4M0\agohd <agohdasol@gmail.com>
2021-01-28 23:04:55 +0900

    ASP.NET ~22장

---
+++ DevSignalR/Hubs/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace DevSignalR.Hubs
+{
+  [HubName("chat")]
+  public class ChatHub : Hub
+  {
+    public void ClientToServer(string msg)
+    {
+      Clients.All.serverToClient(msg);
+    }
+  }
+}
